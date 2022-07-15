a3964eb39ced0b212c85e37961dc8506cd6ca805
Adam Ingwersen Linnemann <adam@Adams-MacBook-Pro.local>
2022-07-15 16:32:23 +0200

    Ship Zapier. Experiment with better typings

--- src/integrations/zapier/scan.ts
+++ src/integrations/zapier/scan.ts
@@ -1,11 +1,12 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { Puppeteer, Browser } from 'puppeteer-core';
 import { formatResponse } from '../../libs/utils';
 import { UserAccess, Cookie } from '../../libs/schema';
+import { Puppeteer, Browser, Page } from 'puppeteer-core';
+
 const chromium = require('@sparticuz/chrome-aws-lambda');
 
-const integrationName = 'Figma';
-const entry = 'https://www.figma.com/files';
+const integrationName = 'Zapier';
+const entry = 'https://zapier.com/app/dashboard';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
@@ -20,36 +21,33 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       ignoreHTTPSErrors: true,
     });
 
-    const page = await browser.newPage();
+    const page: Page = await browser.newPage();
     await page.setRequestInterception(true);
     await page.setCookie(...cookies);
     await page.setViewport({ width: 1200, height: 800 });
 
-    let sessionState = null;
-    let userState = null;
+    let accounts = null;
 
     page.on('request', request => {
+      // Might be able to do something with request.postData here instead?
       request.continue();
     });
 
     page.on('response', async response => {
-      if (response.url().includes('session/state') && response.status() === 200) {
-        sessionState = await response.json();
-      }
-      if (response.url().includes('user/state') && response.status() === 200) {
-        userState = await response.json();
+      if (response.url().includes('accounts') && response.status() === 200) {
+        accounts = await response.json();
       }
     });
 
     await page.goto(entry, { waitUntil: 'networkidle0' });
 
-    if (!sessionState || !userState) {
+    if (!accounts) {
       return formatResponse(300, 'Payload not intercepted');
     }
 
     browser.close();
 
-    return formatResponse(200, JSON.stringify(sessionState).concat(JSON.stringify(userState)));
+    return formatResponse(200, JSON.stringify(accounts));
   } catch (err) {
     return formatResponse(500, '', err);
   }
