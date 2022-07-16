971b0b9e7151b6c8aff6fa54aead826d88f92198
Adam Ingwersen Linnemann <adam@adams-macbook-pro.home>
2022-07-16 10:21:52 +0200

    Add Metabase integration 🔗

---
+++ src/integrations/metabase/scan.ts
@@ -0,0 +1,67 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { formatResponse } from '../../libs/utils';
+import { UserAccess, Cookie } from '../../libs/schema';
+import { Puppeteer, Browser, Page } from 'puppeteer-core';
+
+const chromium = require('@sparticuz/chrome-aws-lambda');
+
+const integrationName = 'Metabase';
+// const entry = 'https://metabase.app';
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const payload: UserAccess = JSON.parse(event.body || '');
+    const cookies: Cookie[] = payload?.cookies;
+
+    const domainCookie = cookies.find(obj => {
+      return obj.name === 'metabase.SESSION';
+    });
+    const domain = domainCookie?.domain;
+    if (!domain) {
+      return formatResponse(300, 'Could not identify domain cookie');
+    }
+
+    const entry = 'https://'.concat(domain);
+
+    const browser: Browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
+
+    const page: Page = await browser.newPage();
+    await page.setRequestInterception(true);
+    await page.setCookie(...cookies);
+    await page.setViewport({ width: 1200, height: 800 });
+
+    let current = null;
+    let properties = null;
+
+    page.on('request', request => {
+      request.continue();
+    });
+
+    page.on('response', async response => {
+      if (response.url().includes('current') && response.status() === 200) {
+        current = await response.json();
+      }
+      if (response.url().includes('properties') && response.status() === 200) {
+        properties = await response.json();
+      }
+    });
+
+    await page.goto(entry, { waitUntil: 'networkidle0' });
+
+    if (!current || !properties) {
+      return formatResponse(300, 'Payload not intercepted');
+    }
+
+    browser.close();
+
+    return formatResponse(200, JSON.stringify(current).concat(JSON.stringify(properties)));
+  } catch (err) {
+    return formatResponse(500, '', err);
+  }
+};
