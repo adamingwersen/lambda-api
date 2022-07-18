1a8218e82b7b06631665bd8587dbd03f9c0a0b81
Adam Ingwersen Linnemann <adam@adams-macbook-pro.home>
2022-07-18 20:59:09 +0200

    fix stupid error where zapier transitions from v3 in browser to v4 api on headless

--- src/integrations/zapier/scan.ts
+++ src/integrations/zapier/scan.ts
@@ -4,7 +4,13 @@ import {
   APIGatewayProxyResult,
 } from "aws-lambda";
 import { formatResponse, instantiatePuppeteer } from "../../libs/utils";
-import { UserAccess, Cookie } from "../../libs/schema";
+import {
+  UserAccess,
+  Cookie,
+  User,
+  ScanReturn,
+  PaymentPlan,
+} from "../../libs/schema";
 
 const integrationName = "Zapier";
 const entry = "https://zapier.com/app/dashboard";
@@ -14,8 +20,11 @@ export const handler: APIGatewayProxyHandler = async (
 ): Promise<APIGatewayProxyResult> => {
   try {
     // Parse post data
-    const payload: UserAccess = JSON.parse(event?.body ?? "");
-    const cookies: Cookie[] = payload?.cookies;
+    const {
+      cookies,
+      ...payload
+    }: { cookies: Cookie[]; userAccess: Omit<UserAccess, "cookies"> } =
+      JSON.parse(event?.body ?? "");
 
     // Start puppeteer / chrome-aws-lambda
     const [browser, page, error] = await instantiatePuppeteer(cookies);
@@ -23,28 +32,81 @@ export const handler: APIGatewayProxyHandler = async (
 
     // Intercept requests
     page.on("request", (request) => {
-      // Might be able to do something with request.postData here instead?
       request.continue();
     });
 
     // Intercept responses
     let accounts = null;
+    let profiles = null;
+
     page.on("response", async (response) => {
-      if (response.url().includes("accounts") && response.status() === 200) {
+      if (response.url().includes("v3/accounts") && response.status() === 200) {
+        console.log(response.url());
         accounts = await response.json();
+      }
+      if (response.url().includes("v3/profiles") && response.status() === 200) {
+        console.log(response.url());
+        profiles = await response.json();
       }
     });
 
     // Visit page
-    await page.goto(entry, { waitUntil: "networkidle0" });
+    await Promise.all([await page.goto(entry, { waitUntil: "networkidle0" })]);
 
-    if (!accounts) {
+    console.log("Got to page");
+
+    if (!accounts || !profiles) {
       return formatResponse(300, "Payload not intercepted");
     }
 
+    console.log("Passed account/profile check");
+    // console.log({ accounts });
+    // console.log({ profiles });
     browser.close();
 
-    return formatResponse(200, JSON.stringify(accounts));
+    const rawUserData: any[] = profiles?.objects[0]?.roles;
+    const mappedUsers: User[] = rawUserData.map((user) => {
+      return {
+        id: user?.account_id,
+        role: user?.role,
+        isAdmin: user?.role === ("owner" || "admin") ? true : false,
+        email: undefined, // This needs to be fixed (upgrade to paid plan)
+        photoString: undefined,
+      };
+    });
+    console.log("Mapped users");
+
+    const paymentPlan: PaymentPlan = {
+      name: accounts?.objects[0]?.plan?.name,
+      isFree: accounts?.objects[0]?.plan?.name === "Free" ? true : false,
+      price: accounts?.objects[0]?.plan?.name === "Free" ? 0.0 : 0.0, // This needs to be fixed
+      nextInstallment: accounts?.objects[0]?.period_end,
+    };
+
+    console.log("Mapped PP");
+
+    const scanReturn: ScanReturn = {
+      inputData: payload.userAccess,
+      integrationName: integrationName,
+      integrationOrganisationId: accounts?.objects[0]?.id,
+      paymentPlan: paymentPlan,
+      paymentPlanPrice: accounts?.objects[0]?.plan?.name, // This needs to be fixed (upgrade to paid plan)
+      paymentPlanIsActive: false, //accounts?.objects[0]?.plan?.metered_task_pricing,
+      paymentPlanIsTrial: true, //accounts?.objects[0]?.is_paid,
+      integrationUserId: accounts?.objects[0]?.owner?.id,
+      integrationUserName: accounts?.objects[0]?.owner?.name,
+      integrationUserEmail: accounts?.objects[0]?.owner?.email,
+      integrationUserImage: profiles?.objects[0]?.photo_url,
+      integrationUsers: mappedUsers,
+      integrationWorkspaces: undefined,
+      integrationUserRole: undefined,
+      blob: {
+        accounts: JSON.stringify(accounts),
+        profiles: JSON.stringify(profiles),
+      },
+    };
+
+    return formatResponse(200, JSON.stringify(scanReturn));
   } catch (err) {
     return formatResponse(500, "", err);
   }
